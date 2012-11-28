4dc4051919537ea98261ae85dddc0727b54d4080
yuanl <yuanl.lee@gmail.com>
2012-11-28 22:37:39 +0800

    what the hell. you lazy.

--- templates/content-picnews.php
+++ templates/content-picnews.php
@@ -3,12 +3,14 @@
 
         <?php $posts = get_posts(array("post_type" => "slide")); ?>
         <?php foreach ($posts as $post) : setup_postdata($post); ?>
+        <?php if has_post_thumbnail(); ?>
             <div class="item">
                 <?php the_post_thumbnail("slide"); ?>
                 <div class="carousel-caption">
                     <p><?php the_title(); ?></p>
                 </div>
             </div>
+        <?php endif; ?>
         <?php endforeach; ?>
     </div>
     <a href="#slider" class="carousel-control left" data-slide="prev">&lsaquo;</a>
