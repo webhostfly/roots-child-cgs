e3fadf66ec73d3dd44a3715fb9ff090746aa0653
YuanL.Lee <lyuanl@gmail.com>
2013-01-09 15:47:59 +0800

    revert use reponsive besides homepage.

--- lib/custom.php
+++ lib/custom.php
@@ -19,8 +19,10 @@ add_action( 'after_setup_theme', 'cgs_setup' );
 function cgs_scripts() {
 	wp_register_script('cgs_main', get_stylesheet_directory_uri() . '/assets/js/main.js', false, null, false);
 	wp_enqueue_script('cgs_main');
-	wp_dequeue_style( 'roots_bootstrap_responsive' );
-	wp_deregister_style('roots_bootstrap_responsive');	
+	if ( is_home() ) {	//do not use responsive in homepage.
+		wp_dequeue_style( 'roots_bootstrap_responsive' );
+		wp_deregister_style('roots_bootstrap_responsive');
+	}
 }
 add_action('wp_enqueue_scripts', 'cgs_scripts', 101);
 
