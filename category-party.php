fe68016e87c82f1f6d2f8cbbdb76e2e884f74d5a
YuanL.Lee <lyuanl@gmail.com>
2013-05-07 17:39:50 +0800

    hide some div in category-party.

--- category-party.php
+++ category-party.php
@@ -1,3 +1,15 @@
+<style>
+#top_nav, #suggest_btn, #topnavbar form {
+	visibility: hidden;
+}
+#header {
+	display: none;
+}
+.container .navbar {
+	display: none;
+}
+</style>
+
 <div class="party">
 	<?php get_template_part('templates/page', 'header'); ?>
 	<?php get_template_part('templates/content', 'party'); ?>
