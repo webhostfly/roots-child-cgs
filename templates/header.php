e4c7b96b6bc612f05f3dec9d1f662d4f4c2b178d
YuanL.Lee <lyuanl@gmail.com>
2013-07-25 09:55:42 +0800

    fwwh preview.

--- templates/header.php
+++ templates/header.php
@@ -1,5 +1,5 @@
 <div id="header" class="row">
     <div class="span12">
-        <h1>北京南航地服</h1>
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/collase.jpg" alt="">
     </div>
 </div> <!-- header -->
